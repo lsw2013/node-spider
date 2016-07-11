c6f56fe7246e5d39310be55089abcd9db15cd1c5
刘尚威 <861044299@qq.com>
2016-07-11 21:17:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,27 @@ var request = require('request');
 var cheerio = require('cheerio');
 var log4js = require('log4js');
 
-var config = require('./config').config;
-// console.log(config);
-var urlLog = config.urlLog;
-var bodyLog = config.bodyLog;
+var urlLog = log.getLogger(1, 'urlLog');
+var bodyLog = log.getLogger(0, 'bodyLog');
 
 var options = {
-    url: 'http://jandan.net',
+    url: 'http://jandan.net/ooxx/page-1',
     headers: {
         'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
-    }
+    };
 };
 
 request(options, function (error, response, body) {
     urlLog.info('url: ' + options.url + '\nresponse.statusCode: ' + response.statusCode);
     if (!error && response.statusCode == 200) {
         // console.log(body);    //返回请求页面的HTML
-        bodyLog.info(body);
+        // bodyLog.info(body);
     } else {
         console.log('error: ' + error);
         console.log('response.statusCode：' + response.statusCode);
         console.log(body);
     };
 });
-
 
 
 
