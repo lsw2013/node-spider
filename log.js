b5c87e76a9c093730a0bd16fd2d7f99335069a97
刘尚威 <861044299@qq.com>
2016-07-11 15:51:03 +0800

    Update log.js

--- log.js
+++ log.js
@@ -8,32 +8,72 @@ var Logger = function () {
     this._name = null;
 };
 
+var _loggers = {};
+
 Logger.getLogger = function (type, name, level) {
     name = String(name || '_');
     level = level || 'DEBUG';
     var logConfig = {};
-    if (!type) {
-        log4js.addAppender(log4js.appenderMakers['file']({
-            filename: name + '.log',
-            maxLogSize: 1000000000,
-            backups: 0
-        }), name);
-    } else {
-        log4js.addAppender(log4js.appenderMakers['datefile']({
-            filename: name,
-            pattern: '.yyyy-MM-dd.log',
-            alwaysIncludePattern: true
-        }), name);
-    };
-    var logger = log4js.getLogger(name);
-    logger.setLevel(level);
+    var logger = _loggers[name];
+    if (!logger) {
+        logger = new Logger();
+        logger._name = name;
+        if (!type) {
+            log4js.addAppender(log4js.appenderMakers['file']({
+                filename: name + '.log',
+                maxLogSize: 1000000000,
+                backups: 0
+            }), name);
+            logger._logger = log4js.getLogger(name);
+            logger._logger.setLevel(logConfig.level);
+        } else {
+            log4js.addAppender(log4js.appenderMakers['datefile']({
+                filename: name,
+                pattern: '.yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            }), name);
+            logger._logger = log4js.getLogger(name);
+            logger._logger.setLevel(logConfig.level);
+        };
+        _loggers[name] = logger;
+    }
     return logger;
-}
+};
 
-var log = Logger.getLogger(1, 'llog');
-log.info('haha');
+Logger.prototype.debug = function(log) {
+	// debug的log
+	this._logger.debug(log);
+};
+
+Logger.prototype.info = function(log) {
+	this._logger.info(log);
+};
+
+Logger.prototype.warn = function(log) {
+	this._logger.warn(log);
+};
+
+Logger.prototype.error = function(log, err) {
+	err = err || {};
+	this._logger.error(log);
+	// 如果err是个对象，其中可能含有message或者stack这两种属性
+	if (err.message) {
+		this._logger.error(err.message);
+	}
+	if (err.stack) {
+		this._logger.error(err.stack);
+	}
+};
 
 module.exports = Logger;
+
+
+
+
+
+
+
+
 
 
 
