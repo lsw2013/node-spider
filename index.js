dafd0b46b71a9259f431ecbfd3595cda2cb1c606
shine <861044299@qq.com>
2016-08-18 12:14:40 +0800

    获取了所有的大图的链接

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ var request = require('request');
 var cheerio = require('cheerio');
 var log4js = require('log4js');
 
-var urlLog = log.getLogger(1, 'urlLog');
-var bodyLog = log.getLogger(0, 'bodyLog');
+// var urlLog = log.getLogger(1, 'urlLog');
+// var bodyLog = log.getLogger(0, 'bodyLog');
 
 var options = {
-    url: 'http://jandan.net/ooxx/page-1',
+    url: 'http://jandan.net/ooxx/page-2096#comments',
     headers: {
         'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
     }
 };
 
 request(options, function (error, response, body) {
-    urlLog.info('url: ' + options.url + '\nresponse.statusCode: ' + response.statusCode);
+    // urlLog.info('url: ' + options.url + '\nresponse.statusCode: ' + response.statusCode);
     if (!error && response.statusCode == 200) {
-        // console.log(body);    //返回请求页面的HTML
-        // bodyLog.info(body);
+    	var $ = cheerio.load(body);
+    	var imgSrcArr = [];
+    	// 获取所有大图
+    	$(".commentlist .text p a").map(function(i,el) {
+    	 	imgSrcArr.push($(this).attr("href"));
+    	});
     } else {
         console.log('error: ' + error);
         console.log('response.statusCode：' + response.statusCode);
         console.log(body);
     };
 });
-
-
 
 
 
